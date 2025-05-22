5f9afc26c6fade0ecbe0b61712a3532cd7df78a5
Hugonofre <hugoant7@gmail.com>
2025-05-21 22:24:33 -0600

    "Actualizando js de proyecto"

--- T-rex/app.js
+++ T-rex/app.js
@@ -1,20 +1,69 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const dino = document.getElementById(".dino");
-    const desert = document.getElementById("#desert");
+document.addEventListener("DOMContentLoaded", function() {
+    const dino = document.getElementById('.dino')
+    const grid = document.getElementById('.grid')
+    const alert = document.getElementById('alert')
+    let gravity = 0.9
+    let isJumping = false
+    let isGameOver = false
 
-    function control() {
-        if (e.keyCode === "Space") {
-            console.log("Jump");
-            jump();
+    function control(e) {
+        if (e.code === "Space") {
+            if (!isJumping) {
+                jump()
+            }
         }
-    }
+    }  
+    
+    let position = 0
     function jump() {
-        let timerId = setInterval(function(){
+        isJumping = true 
+        let count = 0
+        let timerId = setInterval(function() {
+            // move down
+            if (count === 15) {
+                clearInterval(timerId)
+                let downTimerId = setInterval(function(){ 
+                    if (count === 0) {
+                        clearInterval(downTimerId)
+                        isJumping = false
+                    }
+                    position -= 5
+                    count--
+                    position = position * gravity
+                    dino.style.bottom = position + "px"
+                }, 20)
+            }
             // move up
-            
-        })
+            position += 30
+            count++
+            position = position * gravity
+            dino.style.bottom = position + "px"
+        }, 20)
     }
 
-    document.addEventListener("keydown", control);
-});
+    function generateObstacle() {
+        let obstaclePosition = 1000
+        const obstacle = document.createElement('div')
+        obstacle.classList.add('obstacle')
+        grid.appendChild(obstacle)
+        obstacle.style.left = obstaclePosition + 'px'
 
+        let timerId = setInterval(function() {
+            if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+                clearInterval(timerId)
+                alert.innerHTML = "Game Over"
+                isGameOver = true
+                // remove all children
+                while (grid.firstChild) {
+                    grid.removeChild(grid.firstChild)
+                }
+            }
+            obstaclePosition -= 10
+            obstacle.style.left = obstaclePosition + 'px'
+    }, 20)
+    if (!isGameOver) setTimeout(generateObstacle, randomTime)
+    }
+    generateObstacle()
+
+    ducument.addEventListener('keydown', control)
+})
