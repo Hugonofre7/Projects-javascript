24a09730f48c1c4d3ce9bd0234ba4c345684e794
Hugonofre <hugoant7@gmail.com>
2026-06-26 23:30:39 -0600

    add inventory structure and immutable product insertion

---
+++ javascript-backend-engineering/04-objects-and-arrays/inventoryAPI.js
@@ -0,0 +1,45 @@
+const inventory = [
+    {
+        id: 1,
+        nombre: "Laptop",
+        precio: 15000,
+        stock: 5,
+        categoria: "electronica"
+    },
+    {
+        id: 2,
+        nombre: "Teclado",
+        precio: 800,
+        stock: 0,
+        categoria: "accesorios"
+    },
+    {
+        id: 3,
+        nombre: "Monitor",
+        precio: 4500,
+        stock: 3,
+        categoria: "electronica"
+    },
+];
+
+
+function agregarProducto(inventory, producto) {
+    return [...inventory, producto];
+}
+
+const nuevoProducto = {
+    id: 4,
+    nombre: 'Mouse',
+    precio: 350,
+    stock: 10,
+    categoria: 'accesorios'
+}
+
+const inventarioActualizado = agregarProducto(inventory, nuevoProducto)
+
+console.log('=== Inventario original ===')
+console.log(inventory.length)
+
+console.log('=== Inventario actualizado ===')
+console.log(inventarioActualizado.length)
+console.log(inventarioActualizado)
