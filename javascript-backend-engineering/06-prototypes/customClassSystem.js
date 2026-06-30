00d2d9920e76162a5681a1780fc235d5abd56a25
Hugonofre <hugoant7@gmail.com>
2026-06-29 22:56:39 -0600

    add Service class and DatabaseConnection inheritance

--- javascript-backend-engineering/06-prototypes/customClassSystem.js
+++ javascript-backend-engineering/06-prototypes/customClassSystem.js
@@ -10,6 +10,42 @@ class User {
     }
 }
 
+class Service {
+    constructor(name, port, status) {
+        this.name = name;
+        this.port = port;
+        this.status = status;
+    }
+
+    isHealthy() {
+        return this.status === 'active';
+    }
+}
+
+class Connection {
+    constructor(host) {
+        this.host = host;
+    }
+
+    connect() {
+        return `Conectando a ${this.host}`;
+    }
+}
+
+
+class DatabaseConnection extends Connection {
+
+    constructor(host, dbName) {
+        super(host);
+        this.dbName = dbName;
+    }
+
+    connect() {
+        return `Conectando a ${this.host}, base de datos: ${this.dbName}`;
+    }
+}
+
+
 const user1 = new User(1, 'Hugo', 'admin')
 console.log(user1.describe())
 console.log(user1)
@@ -17,3 +53,15 @@ console.log(user1)
 console.log(Object.getPrototypeOf(user1) === User.prototype)
 console.log(user1.hasOwnProperty('describe'))
 console.log(User.prototype.hasOwnProperty('describe'))
+
+const service1 = new Service('auth-service', 3000, 'active')
+const service2 = new Service('payment-service', 3009, 'inactive')
+
+console.log(service1.isHealthy())
+console.log(service2.isHealthy())
+
+const db1 = new DatabaseConnection('localhost:3306', 'epidemiologia')
+
+console.log(db1.connect())
+console.log(db1 instanceof Connection)
+console.log(db1 instanceof DatabaseConnection)
